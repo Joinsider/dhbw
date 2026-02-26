baaa810dac01f54ed4c73c6868e0f9c933ae1a34
Joinsider <public@joinside.de>
2026-02-26 07:40:07 +0100

    Revert "chore: bump version to v2.0.7"
    This reverts commit c99e74b58de4b15bbb244c289cb946f8264f3001.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -110,8 +110,8 @@ android {
         applicationId = "de.fampopprol.dhbwhorb"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 23
-        versionName = "v2.0.7"
+        versionCode = 22
+        versionName = "v2.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
