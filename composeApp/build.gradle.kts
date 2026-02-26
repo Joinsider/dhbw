87e9000c6a3944690022b549bf2e7a1be76e5354
Joinside <43420583+Joinsider@users.noreply.github.com>
2026-02-26 09:17:37 +0100

    Fix keystore (#92)
    * Revert "chore: bump version to v2.0.2"
    
    This reverts commit ccc7069f9bd810141a8f8870f32bdf8a1e707a13.
    
    * Revert "chore: bump version to v2.0.1"
    
    This reverts commit 6055ec98e192d9c128c2fed57852280a5e307e92.
    
    * chore: update build-release.yml to improve keystore decoding process

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -110,8 +110,8 @@ android {
         applicationId = "de.fampopprol.dhbwhorb"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 18
-        versionName = "v2.0.2"
+        versionCode = 16
+        versionName = "v2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
